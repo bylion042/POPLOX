5de4c1e27270a118d23bbae7bf210c4363ecbe99
Bby Lion <codelab042@gmail.com>
2025-06-15 10:06:53 +0100

    first commit

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -6,20 +6,21 @@ router.get('/sitemap.xml', (req, res) => {
 
   res.header('Content-Type', 'application/xml');
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>https://www.poplox.com/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>
-    <url><loc>https://www.poplox.com/login</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/register</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/forgot-password</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/t&c</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/services</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/support</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/contact-us</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/application</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/api-init</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-    <url><loc>https://www.poplox.com/engagement</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
-  </urlset>`;
+ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://www.poplox.com/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>
+  <url><loc>https://www.poplox.com/login</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/register</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/forgot-password</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/t&amp;c</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/services</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/support</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/contact-us</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/application</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/api-init</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+  <url><loc>https://www.poplox.com/engagement</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+</urlset>`;
+
 
   console.log('[SITEMAP] Sitemap generated. Sending response...');
   res.send(sitemap);
